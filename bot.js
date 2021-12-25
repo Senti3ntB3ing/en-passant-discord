28aa9cf8264b213314e781f4c7a76af4c1219947
Cristian A <cristianantonuccio@icloud.com>
2021-12-25 16:59:31 +0100

    fix

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 
-import { createBot } from 'https://deno.land/x/discordeno@13.0.0-rc15/mod.ts';
+import 'https://deno.land/x/discordeno@13.0.0-rc15/mod.ts';
 
 export const bot = createBot({
     botId: Deno.env['ID'],

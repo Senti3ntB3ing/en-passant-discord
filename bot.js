57e041d30bd232b859912285c0be64d4f6711e06
Cristian A <cristianantonuccio@icloud.com>
2021-12-25 17:07:52 +0100

    fix

--- bot.js
+++ bot.js
@@ -2,8 +2,8 @@
 import * as Discord from 'https://deno.land/x/discordeno@13.0.0-rc15/mod.ts';
 
 export const bot = Discord.createBot({
-    botId: Deno.env['ID'],
-    token: Deno.env['TOKEN'],
+    botId: Deno.env.get('ID'),
+    token: Deno.env.get('TOKEN'),
     /* prefix: "!",
     userIDs: {
         botDevs: [ 393073920867041280, 654894296759009316 ],

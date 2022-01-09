e7bc4da9aaac9e64f5af2c8dd8056bf6a7bdac68
Cristian A <cristianantonuccio@icloud.com>
2022-01-09 17:26:26 +0100

    test

--- commands/fen.js
+++ commands/fen.js
@@ -1,7 +1,7 @@
 
 import { Roles } from '../config.js';
 import { createCommand } from '../parser.js';
-import { diagram } from '../components/diagram/diagram.js';
+//import { diagram } from '../components/diagram/diagram.js';
 
 createCommand({
 	name: 'fen', emoji: '📄',
@@ -15,7 +15,8 @@ createCommand({
 			color: 0x000000,
 			file: {
 				blob: new Blob(
-					diagram('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2')
+					//diagram('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2')
+					Deno.readFileSync("../components/diagram/resources/board.png")
 				),
 				name: "board.png",
 				image: { url: 'attachment://board.png' }

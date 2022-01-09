d4daf3ad1a3fe28f3f3788ea65c6409d42fb7362
Cristian A <cristianantonuccio@icloud.com>
2022-01-09 19:27:34 +0100

    quick fix

--- commands/fen.js
+++ commands/fen.js
@@ -1,7 +1,7 @@
 
 import { Roles } from '../config.js';
 import { createCommand } from '../parser.js';
-//import { diagram } from '../components/diagram/diagram.js';
+import { diagram } from '../components/diagram/diagram.js';
 
 createCommand({
 	name: 'fen', emoji: '📄',
@@ -11,8 +11,7 @@ createCommand({
 	execute: _ => ({
 		file: {
 			blob: new Blob(
-				//diagram('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2')
-				[ Deno.readFileSync('./components/diagram/resources/board.png') ]
+				[ diagram('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2') ]
 			),
 			name: 'board.png',
 		},

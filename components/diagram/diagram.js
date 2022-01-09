5881c2e40eb33b10a8209fbce95c9bae58e04374
Cristian A <cristianantonuccio@icloud.com>
2022-01-09 19:25:51 +0100

    fix

--- components/diagram/diagram.js
+++ components/diagram/diagram.js
@@ -1,23 +1,23 @@
 
 import { Chess } from './chess.js';
 
-import { createCanvas, loadImage } from 'https://deno.land/x/canvaseno/mod.ts'
+import { decode } from 'https://deno.land/x/imagescript@1.2.9/mod.ts'
 
-const Board = Deno.readFileSync('./resources/board.png');
+const Board = await decode(Deno.readFileSync('./resources/board.png'));
 
 const Pieces = {
-    'bp': Deno.readFileSync('./resources/alpha/bp.png'),
-    'bn': Deno.readFileSync('./resources/alpha/bn.png'),
-    'bb': Deno.readFileSync('./resources/alpha/bb.png'),
-    'bq': Deno.readFileSync('./resources/alpha/bq.png'),
-    'bk': Deno.readFileSync('./resources/alpha/bk.png'),
-    'br': Deno.readFileSync('./resources/alpha/br.png'),
-    'wp': Deno.readFileSync('./resources/alpha/wp.png'),
-    'wn': Deno.readFileSync('./resources/alpha/wn.png'),
-    'wb': Deno.readFileSync('./resources/alpha/wb.png'),
-    'wq': Deno.readFileSync('./resources/alpha/wq.png'),
-    'wk': Deno.readFileSync('./resources/alpha/wk.png'),
-    'wr': Deno.readFileSync('./resources/alpha/wr.png'),
+    'bp': await decode(Deno.readFileSync('./resources/alpha/bp.png')),
+    'bn': await decode(Deno.readFileSync('./resources/alpha/bn.png')),
+    'bb': await decode(Deno.readFileSync('./resources/alpha/bb.png')),
+    'bq': await decode(Deno.readFileSync('./resources/alpha/bq.png')),
+    'bk': await decode(Deno.readFileSync('./resources/alpha/bk.png')),
+    'br': await decode(Deno.readFileSync('./resources/alpha/br.png')),
+    'wp': await decode(Deno.readFileSync('./resources/alpha/wp.png')),
+    'wn': await decode(Deno.readFileSync('./resources/alpha/wn.png')),
+    'wb': await decode(Deno.readFileSync('./resources/alpha/wb.png')),
+    'wq': await decode(Deno.readFileSync('./resources/alpha/wq.png')),
+    'wk': await decode(Deno.readFileSync('./resources/alpha/wk.png')),
+    'wr': await decode(Deno.readFileSync('./resources/alpha/wr.png')),
 };
 
 export async function diagram(fen) {
@@ -26,21 +26,17 @@ export async function diagram(fen) {
 
     const board = Chess(fen).board();
 
-    const canvas = createCanvas(800, 800);
-    const ctx = canvas.getContext('2d');
-
-    // drawing board background:
-    ctx.drawImage(await loadImage(Board), 0, 0);
+    const canvas = Board.clone();
 
     // drawing pieces:
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
             if (board[i][j] == null) continue;
             const piece = Pieces[board[i][j].color + board[i][j].type];
-            ctx.drawImage(await loadImage(piece), j * 100, i * 100);
+            canvas.composite(piece, j * 100, i * 100);
         }
     }
 
-    return canvas.toBuffer();
+    return canvas.encode();
 
 }

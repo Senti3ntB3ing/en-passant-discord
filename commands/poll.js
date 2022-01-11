128bf8125182d73a415d89395236f77b8dd5495c
Cristian A <cristianantonuccio@icloud.com>
2022-01-11 15:08:34 +0100

    quick fix

--- commands/poll.js
+++ commands/poll.js
@@ -1,19 +1,27 @@
 
-import { ColorCode, Roles } from '../config.js';
-import { createCommand, card } from '../parser.js';
+import { Roles } from '../config.js';
+import { createCommand, card, error } from '../parser.js';
+
+import { sendMessage, addReaction } from 'https://deno.land/x/discordeno@13.0.0-rc18/mod.ts';
 
 createCommand({
 	name: 'poll', emoji: '📊', hidden: true,
 	description: 'Make a poll with the given text and emojis.',
 	permissions: Roles.moderator,
-	execute: message => {
-        const text = message.replace(/^(.*?)\s+/gm, '').trim();
+	execute: async (message, bot) => {
+        const text = message.content.replace(/^(.*?)\s+/gm, '').trim();
 		if (text == '') return card(
 			'Poll Command Help',
 			'ℹ️ You must provide text with some emojis for the poll.'
 		);
 		// extract emojis:
-		const emojis = text.match(/<(a)?:.+?:(\d+)>/g);
-		console.log(text);
+		const emojis = text.match(/[\p{Emoji}\u200d]+/gu);
+		try {
+			const id = await sendMessage(bot, message.channelId, text).id;
+			for (const e of emojis) await addReaction(bot, message.channelId, id, e);
+		} catch (e) {
+			return error('Poll Command', '❌ Error: detected invalid emojis!');
+		}
+		return undefined;
     }
 });

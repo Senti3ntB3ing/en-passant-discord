853aad8fe2799422a99f9ac52cc291b2dd3ca641
Cristian A <cristianantonuccio@icloud.com>
2022-01-20 21:41:01 +0100

    lichess is back

--- commands/rating.js
+++ commands/rating.js
@@ -1,16 +1,11 @@
 
 import { Prefix, Roles } from '../config.js';
 import { createCommand, info, card, cards, warn, error } from '../parser.js';
-import { getLichessUser } from '../components/lichess.js';
+import { getLichessRapidRating, verifyLichessUser } from '../components/lichess.js';
 
 import { Database } from '../database.js';
 
 import { addRole, removeRole } from 'https://deno.land/x/discordeno@13.0.0-rc18/mod.ts';
-
-function normalize(rating) {
-	if (rating == null || rating == undefined) 'UNR';
-	return rating.toString();
-}
 
 const colors = {
 	'FIDE': 0xF1C40F,
@@ -19,72 +14,67 @@ const colors = {
 };
 
 createCommand({
-	name: 'rating', emoji: '⭐️',
-	aliases: [ 'elo' ],
-	description: 'Shows your online rapid Elo rating.',
+	name: 'accounts', emoji: '⭐️',
+	description: 'Shows your linked online chess accounts.',
 	permissions: Roles.moderator,
 	execute: async message => {
-		const userData = await Database.get(message.member.id);
-		if (userData == null) {
-			await Database.set(message.member.id, {
-				accounts: [
-					{
-						platform: 'lichess.org',
-						username: 'KnightShadow98',
-					}
-				]
-			});
-			return info(
-				'Rating Command',
-				'ℹ️ You must link your account to use this command.'
-			);
-		} else {
-			const lichess = await getLichessUser(userData.accounts[0].username);
-			if (lichess == null) return warn(
-				'Rating Command',
-				':warning: No lichess user found with the username `' + text + '`!'
-			);
-			if (lichess.profile == undefined ||
-				lichess.profile.location == undefined ||
-				lichess.profile.location.replace(/\s+/g, '') != username.replace(/\s+/g, '') ||
-				lichess.perfs == undefined ||
-				lichess.perfs.rapid == undefined ||
-				lichess.perfs.rapid.rating == undefined) return error(
-				'Rating Command',
-				'❌ Error: connection with __lichess.org__ failed!'
-			);
-			const rating = normalize(parseInt(lichess.perfs.rapid.rating));
-			return card(
-				'Rating Command',
-				`⭐️ Your __lichess.org__ rapid rating is \`${rating}\`!`,
-				colors['lichess.org']
-			);
-		}
-		return cards();
-		/*const color = colors[platform];
-		if (platform == 'FIDE') platform = '**FIDE**';
-		else platform = `__${platform}__`;*/
-	}
-});
-/*
-createCommand({
-	name: 'unlink', emoji: '⚡️', hidden: true,
-	description: 'Unlinks your online rapid Elo rating.',
-	permissions: Roles.everyone,
-	execute: async (message, bot) => {
-		let platform = findPlatform(message.member.roles);
-		if (platform == null) return info(
-			'Rating Command',
-			'ℹ️ You must link your account to use this command.'
+		const title = 'Accounts Command';
+		const member = await Database.get(message.member.id);
+		if (member == null || member.accounts == undefined) return info(
+			title, 'ℹ️ You must link your account to use this command.'
 		);
-		for (const rating in Roles.ratings) {
-			if (message.member.roles.includes(Roles.ratings[rating])) {
-				await removeRole(bot, message.guildId, message.member.id, Roles.ratings[rating]);
+		let list = [];
+		for (let { platform, username } of member.accounts) {
+			switch (platform) {
+				case 'lichess.org':
+					const rating = await getLichessRapidRating(username);
+					if (rating === undefined) return warn(
+						title, ':warning: No __lichess.org__ user found with the username `' + username + '`!'
+					);
+					if (rating === null) return error(
+						title, '❌ Error: connection with __lichess.org__ failed!'
+					);
+					list.push({
+						title,
+						message: `⭐️ <@${message.member.id}> aka \`${username}\`, your __lichess.org__ rapid rating is \`${rating}\`!`,
+						color: colors[platform]
+					});
 				break;
+				/*case 'chess.com':
+					list.push({
+						title,
+						message: `⭐️ Your __chess.com__ rapid rating is \`${rating}\`!`,
+						color: colors[platform]
+					});
+				break;
+				case 'FIDE': break;*/
 			}
 		}
-		await removeRole(bot, message.guildId, message.member.id, Roles.platforms[platform]);
-		return info('Rating Command', 'ℹ️ You have successfully unlinked your profile.');
+		if (list.length > 0) return cards(list);
+		return info(
+			title, 'ℹ️ You must link your account to use this command.'
+		);
+	}
+});
+
+createCommand({
+	name: 'unlink', emoji: '⚡️', hidden: true,
+	description: 'Unlinks all of your online chess accounts.',
+	permissions: Roles.everyone,
+	execute: async (message, bot) => {
+		const title = 'Unlink Command';
+		let member = await Database.get(message.member.id);
+		if (member == null) return info(
+			title, 'ℹ️ You have `0` linked accounts at the moment.'
+		);
+		member.accounts = [];
+		await Database.set(message.member.id, member);
+		for (const platform in Roles.platforms) {
+			const role = Roles.platforms[platform];
+			if (message.member.roles.includes(role))
+				await removeRole(bot, message.guildId, message.member.id, role);
+		}
+		return info(title, 'ℹ️ You have successfully unlinked your profile.');
 	}
 });
 
@@ -94,51 +84,51 @@ createCommand({
 	description: 'Link your __lichess.org__ rapid rating.',
 	permissions: Roles.everyone,
 	execute: async (message, bot) => {
-		const platform = findPlatform(message.member.roles);
-		if (platform != null && platform != 'lichess.org') return warn(
-			'Rating Command',
-			':warning: Your rating is already linked with __' + platform + '__.\n' +
-			'If you want to use a different platform `!unlink` this one first.'
-		);
-		if (platform == null)
-			await addRole(bot, message.guildId, message.member.id, Roles.platforms['lichess.org']);
+		const title = 'Rating Command';
 		const text = message.content.replace(/^(.*?)\s+/gm, '').trim();
 		const username = message.tag;
-		if (text == Prefix + 'lichess' || text == Prefix + 'lichess.org') return info(
-			'Rating Command',
-			'ℹ️ Go on your __lichess.org__ settings page and add your **Discord** username (`' +
-			username + '`) to the `location` field.\n' +
-			'Type `!lichess your_lichess_username` to link your account.\n' +
-			'If your **Discord** username contains spaces or symbols it might not work, ' +
-			'but you can always ask a moderator to manually fix it for you.'
-		);
-		const lichess = await getLichessUser(text);
-		if (lichess == null) return warn(
-			'Rating Command',
-			':warning: No lichess user found with the username `' + text + '`!'
-		);
-		if (lichess.profile == undefined ||
-			lichess.profile.location == undefined ||
-			lichess.profile.location.replace(/\s+/g, '') != username.replace(/\s+/g, '') ||
-			lichess.perfs == undefined ||
-			lichess.perfs.rapid == undefined ||
-			lichess.perfs.rapid.rating == undefined) return error(
-			'Rating Command',
-			'❌ Error: connection with __lichess.org__ failed!'
-		);
-		const rating = normalize(parseInt(lichess.perfs.rapid.rating));
-		for (const elo in Roles.ratings) {
-			if (message.member.roles.includes(Roles.ratings[elo])) {
-				await removeRole(bot, message.guildId, message.member.id, Roles.ratings[elo]);
-				break;
-			}
+		let member = await Database.get(message.member.id);
+		if (text == Prefix + 'lichess' || text == Prefix + 'lichess.org') {
+			const process = info(
+				'Rating Command',
+				'ℹ️ Go on your __lichess.org__ settings page and add your **Discord** username (`' +
+				username + '`) to the `location` field.\n' +
+				'Type `!lichess your_lichess_username` to link your account.\n' +
+				'If your **Discord** username contains spaces or symbols it might not work, ' +
+				'but you can always ask a moderator to manually fix it for you.'
+			);
+			if (member == null || member.accounts == undefined) return process;
+			const lichess = member.accounts.find(a => a.platform == 'lichess.org');
+			if (lichess == undefined) return process;
+			const rating = await getLichessRapidRating(lichess.username);
+			if (rating === undefined) return warn(
+				title, ':warning: No __lichess.org__ user found with the username `' + lichess.username + '`!'
+			);
+			if (rating === null) return error(
+				title, '❌ Error: connection with __lichess.org__ failed!'
+			);
+			return card(
+				title,
+				`⭐️ <@${message.member.id}> aka \`${lichess.username}\`, your __lichess.org__ rapid rating is \`${rating}\`!`,
+				colors['lichess.org']
+			);
 		}
-		await addRole(bot, message.guildId, message.member.id, Roles.ratings[rating]);
-		return card(
+		if (member == null) member = { accounts: [ ] };
+		const verified = await verifyLichessUser(text);
+		if (verified == undefined) return warn(
+			title, ':warning: No lichess user found with the username `' + text + '`!'
+		);
+		if (!verified) return error(
 			'Rating Command',
-			'✅ Your rating has been linked with __lichess.org__!',
+			'❌ Error: verification with __lichess.org__ failed!'
+		);
+		member.accounts.push({ platform: 'lichess.org', username: text });
+		await Database.set(message.member.id, member);
+		await addRole(bot, message.guildId, message.member.id, Roles.platforms['lichess.org']);
+		return card(
+			title,
+			'✅ Successfully verified! Type `!lichess` to see your rating.',
 			colors['lichess.org']
 		);
 	}
 });
-*/
--- components/lichess.js
+++ components/lichess.js
@@ -7,3 +7,24 @@ export async function getLichessUser(user) {
 		return await response.json();
 	} catch { return null; }
 }
+
+export async function getLichessRapidRating(user) {
+	const lichess = await getLichessUser(user);
+	if (lichess == null) return undefined;
+	if (lichess.perfs == undefined ||
+		lichess.perfs.rapid == undefined ||
+		lichess.perfs.rapid.rating == undefined) return null;
+	if (isNaN(parseInt(lichess.perfs.rapid.rating))) return 'unrated';
+	return lichess.perfs.rapid.rating;
+}
+
+export async function verifyLichessUser(name, discord) {
+	const lichess = await getLichessUser(name);
+	if (lichess == null) return undefined;
+	discord = discord.replace(/\s+/g, '');
+	return !(
+		lichess.profile == undefined ||
+		lichess.profile.location == undefined ||
+		lichess.profile.location.replace(/\s+/g, '') != discord
+	);
+}

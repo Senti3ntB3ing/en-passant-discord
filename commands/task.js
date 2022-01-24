0b24d0cf149133ef3f25944cf5ffd319234074a6
Cristian A <cristianantonuccio@icloud.com>
2022-01-24 18:11:13 +0100

    quick fix

--- commands/task.js
+++ commands/task.js
@@ -1,6 +1,6 @@
 
 import { Roles } from '../config.js';
-import { createCommand, tasks } from '../parser.js';
+import { createCommand, tasks, error, info } from '../parser.js';
 
 createCommand({
 	name: 'task', emoji: '⏳',
@@ -12,7 +12,9 @@ createCommand({
 		else if (message.arguments.length != 1)
 			return error('Task Command', 'Expected a valid task name.');
 		const name = message.arguments[0];
-		if (name in tasks) tasks[name].execute(message.bot);
-		else return error('Task Command', `Task \`${name}\` not found.`);
+		if (name in tasks) {
+			tasks[name].execute(message.bot);
+			return card('Task Command', `Task \`${name}\` executed.`);
+		} else return error('Task Command', `Task \`${name}\` not found.`);
 	}
 });

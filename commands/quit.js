924c1e62cbd527f0fd303bb8ac4841856476afc3
Cristian A <cristianantonuccio@icloud.com>
2022-01-24 22:53:10 +0100

    quick fix

--- commands/quit.js
+++ commands/quit.js
@@ -1,11 +1,18 @@
 
+import { sendMessage } from 'https://deno.land/x/discordeno@13.0.0-rc18/mod.ts';
+
 import { Roles } from '../config.js';
-import { createCommand } from '../parser.js';
+import { createCommand, error } from '../parser.js';
 
 createCommand({
 	name: 'quit', emoji: '🔌', hidden: true,
 	aliases: [ 'shutdown', 'selfdestruct', 'self-destruct' ],
 	description: 'Shutdown the bot.',
 	permissions: Roles.moderator,
-	execute: () => { Deno.exit(1); }
+	execute: async message => {
+		await sendMessage(message.bot, message.channelId, error(
+			'Self Destruction', 'Shutting the system down...'
+		));
+		Deno.exit(1);
+	}
 });

e2d642dd7084bf636537c21e2f7aa0d8d15c0fb3
Cristian A <cristianantonuccio@icloud.com>
2022-03-08 13:32:05 +0100

    fixes

--- commands/backup.js
+++ commands/backup.js
@@ -6,11 +6,11 @@ import { Database } from '../database.js';
 createCommand({
 	name: 'backup', emoji: ':radioactive:', hidden: true,
 	description: 'Backup of the entire **BOT** database.',
-    permissions: Roles.moderator,
+	permissions: Roles.moderator,
 	execute: async () => ({
-        file: {
-            blob: new Blob([ JSON.parse(await Database.dictionary()) ]),
-            name: 'en-passant.json',
-        }
+		file: {
+			blob: new Blob([ JSON.parse(await Database.dictionary()) ]),
+			name: 'en-passant.json',
+		}
 	})
 });

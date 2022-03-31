8204b8d3274351eac6a1c8349ac12e5814055b81
Cristian-A <cristianantonuccio@icloud.com>
2022-03-31 16:29:50 +0200

    massive fix

--- tasks/twitch.js
+++ tasks/twitch.js
@@ -4,15 +4,29 @@ import { sendMessage } from 'https://deno.land/x/discordeno@13.0.0-rc18/mod.ts';
 import { live } from '../components/twitch.js';
 import { Roles, Time, Twitch_Streamer } from '../config.js';
 import { createTask, card } from '../parser.js';
+import { Database } from '../database.js';
+
+const Twitch = {
+	url: 'https://www.twitch.tv/thechessnerdlive/',
+	color: 0x9047FF, emoji: ':gem:'
+};
 
 createTask({
 	name: 'twitch',
 	interval: Time.minutes(5),
 	execute: async bot => {
-		if (await live(Twitch_Streamer)) sendMessage(bot, Channels.notifications, card(
-			'Zach is now live on Twitch!',
-			`${links['Twitch'].emoji} Hey @everyone, <@${Roles.Zach}> is streaming on __twitch__!\n${links['Twitch'].url}`,
-			links['Twitch'].color
-		));
+		// if streaming already: update state and don't do anything.
+		// else if live: update state and send notification.
+		if (await Database.get('twitch_live')) {
+			Database.set('twitch_live', await live(Twitch_Streamer));
+		} else if (await live(Twitch_Streamer)) {
+			Database.set('twitch_live', true);
+			sendMessage(bot, Channels.notifications, card(
+				'Zach is now live on Twitch!',
+				`${Twitch.emoji} Hey @everyone, <@${Roles.Zach}> is streaming on __twitch__!` +
+				`\n${Twitch.url}`,
+				Twitch.color
+			));
+		}
 	}
 });

7030d1b5b39aeb134553b66c88eb550f4e4ba215
Cristian-A <cristianantonuccio@icloud.com>
2022-05-01 16:38:57 +0200

    fix

--- tasks/move.js
+++ tasks/move.js
@@ -3,6 +3,7 @@ import { sendMessage, deleteMessage } from 'https://deno.land/x/discordeno@13.0.
 
 import { createTask } from '../parser.js';
 import { Roles, Channels, Time } from '../config.js';
+import { Chess } from '../components/chess.js';
 import { playing, game, getGame, setGame, endGame, clearVotes, moves } from '../components/votechess.js';
 import { stateMessage } from '../components/diagram/diagram.js';
 

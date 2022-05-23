1433846728f212e3d478ae83244f428da0be9a4a
Cristian-A <cristianantonuccio@icloud.com>
2022-05-23 17:26:50 +0200

    fix

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -1,8 +1,6 @@
 
-import { sendMessage } from 'https://deno.land/x/discordeno@13.0.0-rc34/mod.ts';
-
 import { Roles } from '../config.js';
-import { createCommand, error } from '../parser.js';
+import { createCommand, send, error } from '../parser.js';
 
 createCommand({
 	name: 'shutdown', emoji: ':firecracker:', hidden: true,
@@ -10,8 +8,8 @@ createCommand({
 	description: 'Shutdown the bot.',
 	permissions: Roles.moderator,
 	execute: async command => {
-		await sendMessage(command.bot, command.channelId, error(
-			'Self Destruction', 'The system has completed the self-destruction sequence.'
+		await send(command.channelId, error(
+			'Shutdown', 'The system is now offline.'
 		));
 		Deno.exit(1);
 	}

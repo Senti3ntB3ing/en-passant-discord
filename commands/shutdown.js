cc3b7db0cf17ac9691b170fc10edb67500f76c7b
Cristian-A <cristianantonuccio@icloud.com>
2022-05-25 16:31:40 +0200

    restart link

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -9,7 +9,10 @@ prefix({
 	permissions: Roles.moderator,
 	execute: async command => {
 		await send(command.channelId, error(
-			'Shutdown', 'The system is now offline.'
+			'Shutdown', 'The system is now offline.\n' +
+			'The bot will be restarted automatically in `5m`.\n' +
+			'If absolutely necessary you can restart it with this link:\n' +
+			'https://en-passant.deno.dev/'
 		));
 		Deno.exit(1);
 	}

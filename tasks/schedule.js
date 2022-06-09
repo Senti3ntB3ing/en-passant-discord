441461386f4673ab9ac7ba773187838c462e1482
Cristian-A <cristianantonuccio@icloud.com>
2022-06-09 18:40:42 +0200

    fix

--- tasks/schedule.js
+++ tasks/schedule.js
@@ -2,11 +2,22 @@
 import { Twitch_Streamer, Channels } from '../config.js';
 import { createTask, send, error, event } from '../parser.js';
 import { channel, schedule } from '../components/twitch.js';
+import { Database } from '../database.js';
 
 createTask({
 	name: 'schedule', emoji: ':calendar_spiral:', time: '12:55',
 	description: 'Adds the streams to the discord events tab.',
 	execute: async () => {
+		// check if today has been fired already
+		const now = new Date();
+		const isToday = date =>
+			date.getDate() == now.getDate() &&
+			date.getMonth() == now.getMonth() &&
+			date.getFullYear() == now.getFullYear();
+		const lastEvent = await Database.get('event');
+		await Database.set('event', now.toISOString());
+		if (lastEvent == null || lastEvent == undefined ||
+			isToday(new Date(lastEvent))) return;
 		// fetch the events on the day 1 week from now:
 		const date_s = new Date(), date_e = new Date();
 		date_s.setDate(date_s.getDate() + 1);

0444e141814ffb9465b1e6bdac49325bdfee9ada
Cristian-A <cristianantonuccio@icloud.com>
2022-06-20 15:34:53 +0200

    fixes

--- actions/queue.js
+++ actions/queue.js
@@ -19,7 +19,7 @@ class Queue {
 		if (this.#queue == null || this.#queue == undefined) return [];
 		return this.#queue;
 	}
-	async push(element) {
+	async enqueue(element) {
 		await queueLock.knock();
 		queueLock.lock();
 		this.refresh();
@@ -31,7 +31,7 @@ class Queue {
 		queueLock.unlock();
 		return position;
 	}
-	async pop() {
+	async dequeue() {
 		await queueLock.knock();
 		queueLock.lock();
 		this.refresh();
@@ -49,6 +49,14 @@ class Queue {
 		queueLock.unlock();
 	}
 
+	async clear() {
+		await queueLock.knock();
+		queueLock.lock();
+		this.#queue = [];
+		await Database.set('queue', this.#queue);
+		queueLock.unlock();
+	}
+
 }
 
 const queue = new Queue();
@@ -59,12 +67,12 @@ programmable({
 	execute: async data => {
 		const username = data.message.match(/join\s+(\w+)/);
 		if (username == null || username.length < 2)
-			return `Try with ${Prefix}join username.`;
-		const index = await queue.push({
+			return `Try with ${Prefix}join <Chess.com username>.`;
+		const index = await queue.enqueue({
 			user: data.username, profile: username[1]
 		});
 		const i = ordinal(index);
-		return `@${data.username} aka ${username[1]} is ${i} in the queue.`;
+		return `@${data.username} aka '${username[1]}' is ${i} in the queue.`;
 	}
 });
 
@@ -78,12 +86,12 @@ programmable({
 });
 
 programmable({
-	commands: [ 'next' ],
+	commands: [ 'next' ], moderator: true,
 	description: 'Get the next in line in the queue.',
 	execute: async () => {
-		const element = await queue.pop();
+		const element = await queue.dequeue();
 		if (element == undefined) return `There is no one in the queue.`;
-		return `@${element.user} aka ${element.profile} is next.`;
+		return `@${element.user} aka '${element.profile}' is next.`;
 	}
 });
 
@@ -93,6 +101,15 @@ programmable({
 	execute: async () => {
 		const list = await queue.refresh();
 		if (list.length == 0) return 'The queue is empty.';
-		return list.map(e => e.profile).join(', ');
+		return 'Queue: ' + list.map(e => e.profile).join(', ');
+	}
+});
+
+programmable({
+	commands: [ 'clear' ], moderator: true,
+	description: 'Clears the current queue.',
+	execute: async () => {
+		await queue.clear();
+		return 'The queue has been cleared.';
 	}
 });

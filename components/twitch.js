528f2cf8bd0d49c0e13e849123f0ae379c92ceb3
Cristian-A <cristianantonuccio@icloud.com>
2022-06-21 00:39:19 +0200

    massive fix

--- components/twitch.js
+++ components/twitch.js
@@ -3,7 +3,7 @@ import { Time } from '../config.js';
 
 const TWITCH_CLIENT_ID = Deno.env.get("TWITCH_CLIENT_ID");
 const TWITCH_AUTH_TOKEN = Deno.env.get("TWITCH_AUTH_TOKEN");
-const TWITCH_OAUTH_TOKEN = Deno.env.get("TWITCH_OAUTH_TOKEN");
+const TWITCH_OAUTH_TOKEN = Deno.env.get("TWITCH_OAUTH_ZACH");
 
 export const BASE_URL = "https://api.twitch.tv/helix/";
 export const QUERIES = {

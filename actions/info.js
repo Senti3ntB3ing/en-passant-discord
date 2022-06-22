1402dab6f0cf7bb2a5e20c93423c431225469351
Cristian-A <cristianantonuccio@icloud.com>
2022-06-22 21:33:56 +0200

    fix

--- actions/info.js
+++ actions/info.js
@@ -27,7 +27,7 @@ programmable({
 	execute: async data => {
 		const args = data.message.split(/\s+/);
 		if (args.length < 2) return;
-		const streamer = args[1];
+		const streamer = args[1].replace(/^@+/, '');
 		return `Follow @${streamer} at twitch.tv/${streamer}`;
 	}
 });
@@ -37,11 +37,11 @@ programmable({
 	description: 'Chess.com terms of service.',
 	execute: async data => {
 		const args = data.message.split(/\s+/);
-		if (args.length < 2)
-			return `Please don't suggest moves for the current position as ` +
+		if (args.length < 2) return
+			`Please don't suggest moves for the current position as ` +
 			`it's against chess.com terms of service. Instead please ask ` +
 			`Zach about a possible move after the position has passed`;
-		const user = args[1];
+		const user = args[1].replace(/^@+/, '');
 		return `@${user} please don't suggest moves for the current position ` +
 			`as it's against chess.com terms of service. Instead please ask ` +
 			`Zach about a possible move after the position has passed`;

eee19921098c34d8c482f46223981d7d7dda8597
Cristian-A <cristianantonuccio@icloud.com>
2022-06-24 21:15:13 +0200

    fix

--- attachments/pgn.js
+++ attachments/pgn.js
@@ -22,7 +22,7 @@ attachment({
 		let b = pgn.match(/\[Black\s+["'](\w+)["']\]/i);
 		if (b != null && b.length > 1) b = b[1]; else b = null;
 		let description = '';
-		if (w != null && b != null) description = `${w} vs ${b}`;
+		if (w != null && b != null) description = `⬜️ **${w}** vs ⬛️ **${b}**`;
 		let status = '';
 		if (game.ended()) {
 			if (game.draw()) status = '½-½ ・ Draw';

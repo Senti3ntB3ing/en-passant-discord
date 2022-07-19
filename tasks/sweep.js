f68ea98377a863301c78649928c3705f1d4aa018
Cristian-A <cristianantonuccio@icloud.com>
2022-07-19 20:16:33 +0200

    sweep fix

--- tasks/sweep.js
+++ tasks/sweep.js
@@ -4,6 +4,6 @@ import { createTask, clear } from '../parser.js';
 
 createTask({
 	name: 'sweep', emoji: ':broom:', time: '00:00',
-	description: `Sweeps <#${Channels.dev_chat}>'s old messages.`,
-	execute: () => clear(Channels.dev_chat, 100)
+	description: `Sweeps <#${Channels.bot_tests}>'s old messages.`,
+	execute: () => clear(Channels.bot_tests, 100)
 });

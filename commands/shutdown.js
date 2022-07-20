6c6fdcda559c552250052a1f08cc29ef0d6fae79
Cristian-A <cristianantonuccio@icloud.com>
2022-07-20 12:59:44 +0200

    added alias

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -3,13 +3,14 @@ import { prefix, send, info, error } from '../parser.js';
 import { PID } from '../main.js';
 
 prefix({
-	name: 'shutdown', emoji: ':firecracker:', aliases: [ 'die' ],
+	name: 'shutdown', emoji: ':firecracker:', aliases: [ 'die', 'kill' ],
 	description: 'Shutdown the bot.',
 	execute: async command => {
 		const m = command.text.match(/(\d+)/);
-		if (m != null && m.length >= 2)
+		if (m != null && m.length >= 2) {
 			if (m[2] == PID) Deno.exit(1);
 			else return;
+		}
 		await send(command.channelId, error(
 			'Shutdown', 'The system is now offline.\n' +
 			'It will be back online in a few minutes.\n' +

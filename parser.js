5a2fe17ff23c067bfdbd9d08ac043705a81e6b1f
Cristian-A <cristianantonuccio@icloud.com>
2022-08-04 18:31:43 +0200

    fix

--- parser.js
+++ parser.js
@@ -60,18 +60,14 @@ export async function handleChesscomGame(type, id, message) {
 	let game = undefined;
 	if (type == 'live') game = await live(id);
 	else game = await daily(id);
-	console.log(id);
 	if (game == undefined) return;
-	console.log(game);
 	const board = new Chess(game.pgnHeaders.FEN);
-	console.log('j');
 	for (const move of game.moveList) if (board.move(move) == null) return;
 	const data = await gif(board);
 	const w = game.pgnHeaders.White, b = game.pgnHeaders.Black;
 	let description = `⬜️ **\`${w}\`** vs **\`${b}\`** ⬛️`;
 	description += ` ・ \`${control(game.pgnHeaders.TimeControl)}\``;
 	let status = '';
-	console.log(`in here2`);
 	if (game.isFinished) {
 		status = game.pgnHeaders.result.replace(/1\/2/, '½').replace(/\-/, '-');
 		if (game.pgnHeaders.Termination != undefined) status += (' ・ ' +

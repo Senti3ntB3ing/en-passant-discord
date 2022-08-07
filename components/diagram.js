9405a557dd87286fb490175334646373d2f3a8f0
Cristian-A <cristianantonuccio@icloud.com>
2022-08-07 15:34:37 +0200

    test

--- components/diagram.js
+++ components/diagram.js
@@ -3,14 +3,12 @@ import { Position, gif as encoder }
 from 'https://deno.land/x/beta_chess_diagrams@v2.0.0/mod.ts';
 
 export async function gif(game, perspective = 'w') {
-	const moves = game.history(); game.reset();
 	const p = new Position(game.board);
 	const frames = [ await p.frame(perspective, true) ];
-	/*for (const move of moves) {
-		game.move(move);
+	while (game.takeback() != null) {
 		frames.push(await (p.set(game.board).frame(perspective, true)));
-	}*/
-	return encoder(frames);
+	}
+	return encoder(frames.reverse());
 }
 
 export async function diagram(board, perspective = 'w') {

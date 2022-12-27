03c10adb411f421d1727a6b70475cbbe6757e1e0
cristian-5 <cristianantonuccio@icloud.com>
2022-12-27 11:24:33 +0100

    test audit

--- commands/record.js
+++ commands/record.js
@@ -1,5 +1,6 @@
 
-import { command, card, fetchLog, guild } from '../parser.js';
+import { command, card, fetchLog, guild, Option, discriminator } from '../parser.js';
+import { Database } from '../database.js';
 
 command({
 	name: 'record', emoji: ':bookmark_tabs:',
@@ -15,5 +16,41 @@ command({
 		return card('Member Count', `:hash: The server has \`${
 			g.approximateMemberCount
 		}\` total members.`);
+	}
+});
+
+command({
+	name: 'punish', emoji: ':no_entry:', options: [{
+		description: 'Member to punish',
+		name: 'member', type: Option.User, required: true
+	}, {
+		description: 'Type of punishment',
+		name: 'type', type: Option.String, required: true,
+		choices: [
+			{ name: '🚫 Ban', value: 'BAN' },
+			{ name: '🛡️ Safety Flag', value: 'SAFE' },
+			{ name: '👠 Kick', value: 'KICK' },
+			{ name: '🗓️ 1W Timeout', value: '1W-T' },
+			{ name: '🗞️ 1D Timeout', value: '1D-T' },
+			{ name: '🕰️ 1H Timeout', value: '1H-T' },
+			{ name: '🧨 10M Timeout', value: '10M-T' },
+			{ name: '⏲️ 5M Timeout', value: '5M-T' },
+			{ name: '⏱️ 1M Timeout', value: '1M-T' },
+			{ name: '🚸 Warn', value: 'WARN' },
+		]
+	}, {
+		description: 'Reason for punishment',
+		name: 'reason', type: Option.String, required: true
+	}],
+	description: '⛔ Punish a member.',
+	execute: async interaction => {
+		const tag = await discriminator(interaction.data.options[0].value);
+		const punishment = interaction.data.options[1].value;
+		const type = interaction.data.options[1].name;
+		const reason = interaction.data.options[2].value;
+		const audit = (await Database.get('audit')) || [];
+		audit.push({ tag, punishment, reason });
+		await Database.set('audit', audit);
+		return card('Punish', `:no_entry: Punished \`${tag}\` with \`${type}\` for ||\`${reason}\`||.`);
 	}
 });

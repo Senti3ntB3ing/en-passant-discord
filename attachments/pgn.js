973f9ba4bf610aa8accca2512749592785058d74
cristian-5 <cristianantonuccio@icloud.com>
2023-01-10 14:09:14 +0100

    fix pgn

--- attachments/pgn.js
+++ attachments/pgn.js
@@ -19,10 +19,10 @@ attachment({
 		const h = game.header();
 		const history = game.history({ verbose: true }).map(
 			m => (m.flags.includes('e') ? '$' : '') + // en passant
-			(move.san === 'O-O' ? // castling
-				(move.color === 'b' ? 'h8f8e8g8' : 'h1f1e1g1') :
-				(move.san === 'O-O-O' ?
-					(move.color === 'b' ? 'a8d8e8c8' : 'a1d1e1c1') : ( // normal
+			(m.san === 'O-O' ? // castling
+				(m.color === 'b' ? 'h8f8e8g8' : 'h1f1e1g1') :
+				(m.san === 'O-O-O' ?
+					(m.color === 'b' ? 'a8d8e8c8' : 'a1d1e1c1') : ( // normal
 						m.from + m.to + (m.promotion ? '=' + ( // promotion
 							m.color === 'w' ? m.promotion.toUpperCase() : m.promotion.toLowerCase()
 						) : '')

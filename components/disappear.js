88bf5e965194eef6128ac1c60f5e52d3870b4e63
cristian-5 <cristianantonuccio@icloud.com>
2023-01-25 19:43:06 +0100

    fixed disappearing messages

--- components/disappear.js
+++ components/disappear.js
@@ -4,14 +4,11 @@
 import { Database } from "../database.js";
 import { remove, send } from "../parser.js";
 
-export async function disappearing(channel, message, minutes = 10) {
+export async function disappearing(channel, message) {
 	const disappearing = (await Database.get("disappearing")) || [];
 	try {
 		const m = await send(channel, message);
-		disappearing.push({
-			id: m.id.toString(), channel: channel.toString(),
-			minutes, time: Date.now()
-		});
+		disappearing.push({ id: m.id.toString(), channel: channel.toString() });
 	} catch { }
 	Database.set("disappearing", disappearing);
 };
@@ -19,11 +16,9 @@ export async function disappearing(channel, message, minutes = 10) {
 export async function sweep() {
 	const disappearing = (await Database.get("disappearing")) || [];
 	for (const message of disappearing) {
-		const { id, channel, minutes, time } = message;
-		if (Date.now() - new Date(time) >= minutes * 60 * 1000) {
-			try { await remove(BigInt(channel), BigInt(id)); } catch { }
-			disappearing.splice(disappearing.indexOf(message), 1);
-		}
+		const { id, channel } = message;
+		try { await remove(BigInt(channel), BigInt(id)); } catch { }
+		disappearing.splice(disappearing.indexOf(message), 1);
 	}
 	Database.set("disappearing", disappearing);
 }
--- tasks/disappear.js
+++ tasks/disappear.js
@@ -4,7 +4,7 @@ import { createTask } from "../parser.js";
 import { sweep } from "../components/disappear.js";
 
 createTask({
-	name: "disappear", emoji: ":dotted_line_face:", interval: Time.minutes(3),
-	description: "Sweeps disappearing messages.",
+	name: "disappear", emoji: ":dotted_line_face:", interval: Time.minutes(7),
+	description: "Delete disappearing messages.",
 	execute: () => sweep()
 });

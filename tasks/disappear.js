860e6f0bca55b23ebfc66fb6a4e53036e298fbfd
cristian-5 <cristianantonuccio@icloud.com>
2023-01-25 17:33:09 +0100

    fixed missing extension

--- tasks/disappear.js
+++ tasks/disappear.js
@@ -1,6 +1,6 @@
 
 import { createTask } from "../parser.js";
-import { sweep } from "../components/disappear";
+import { sweep } from "../components/disappear.js";
 
 createTask({
 	name: "disappear", emoji: ":dotted_line_face:", interval: Time.minutes(3),

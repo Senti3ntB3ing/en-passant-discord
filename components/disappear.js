2b86f0449fd6ca1c6beb044934bf5751fd83d339
cristian-5 <cristianantonuccio@icloud.com>
2023-01-25 22:25:37 +0100

    swapped channel and id

--- components/disappear.js
+++ components/disappear.js
@@ -17,7 +17,7 @@ export async function sweep() {
 	const disappearing = (await Database.get("disappearing")) || [];
 	for (const message of disappearing) {
 		const { id, channel } = message;
-		try { await remove(BigInt(channel), BigInt(id)); } catch { }
+		try { await remove(BigInt(id), BigInt(channel)); } catch { }
 		disappearing.splice(disappearing.indexOf(message), 1);
 	}
 	Database.set("disappearing", disappearing);

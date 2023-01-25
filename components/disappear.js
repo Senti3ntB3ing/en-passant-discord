63e24804faa2b71d6f99cb85f4e32183fa47ee46
cristian-5 <cristianantonuccio@icloud.com>
2023-01-25 19:19:17 +0100

    bigint serialization

--- components/disappear.js
+++ components/disappear.js
@@ -8,7 +8,10 @@ export async function disappearing(channel, message, minutes = 10) {
 	const disappearing = (await Database.get("disappearing")) || [];
 	try {
 		const m = await send(channel, message);
-		disappearing.push({ id: m.id, channel, minutes, time: Date.now() });
+		disappearing.push({
+			id: m.id.toString(), channel: channel.toString(),
+			minutes, time: Date.now()
+		});
 	} catch { }
 	Database.set("disappearing", disappearing);
 };
@@ -17,8 +20,8 @@ export async function sweep() {
 	const disappearing = (await Database.get("disappearing")) || [];
 	for (const message of disappearing) {
 		const { id, channel, minutes, time } = message;
-		if (Date.now() - time >= minutes * 60 * 1000) {
-			try { await remove(channel, id); } catch { }
+		if (Date.now() - new Date(time) >= minutes * 60 * 1000) {
+			try { await remove(BigInt(channel), BigInt(id)); } catch { }
 			disappearing.splice(disappearing.indexOf(message), 1);
 		}
 	}

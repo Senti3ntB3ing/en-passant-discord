8d0d0836cd2f1c8a26842ad97280646cba0fb5f5
cristian-5 <cristianantonuccio@icloud.com>
2023-04-05 18:45:40 +0200

    removed /punish

--- commands/system.js
+++ commands/system.js
@@ -4,7 +4,6 @@ import {
 	command, send, prefix, card, error, info, tasks,
 	guild, Option, snow
 } from '../parser.js';
-import { Database } from '../database.js';
 import { PID } from '../main.js';
 
 command({
@@ -14,7 +13,8 @@ command({
 		'Ping Command',
 		`:ping_pong: **Pong**. Server latency: \`${
 			Date.now() - snow(interaction.id)
-		}ms\`, PID: \`${PID}\`.`
+		}ms\`, PID: \`${PID}\`.`,
+		undefined, true
 	),
 });
 
@@ -25,45 +25,7 @@ command({
 		const g = await guild(interaction.guildId);
 		return card('Member Count', `:hash: The server has \`${
 			g.approximateMemberCount
-		}\` total members.`);
-	}
-});
-
-command({
-	name: 'punish', emoji: ':no_entry:', options: [{
-		description: 'Member to punish',
-		name: 'member', type: Option.String, required: true
-	}, {
-		description: 'Type of punishment',
-		name: 'type', type: Option.String, required: true,
-		choices: [
-			{ name: '🚫 Ban', value: 'BAN' },
-			{ name: '🛡️ Safety Flag', value: 'SAFE' },
-			{ name: '👠 Kick', value: 'KICK' },
-			{ name: '🗓️ 1W Timeout', value: '1W-T' },
-			{ name: '🗞️ 1D Timeout', value: '1D-T' },
-			{ name: '🕰️ 1H Timeout', value: '1H-T' },
-			{ name: '🧨 10M Timeout', value: '10M-T' },
-			{ name: '⏲️ 5M Timeout', value: '5M-T' },
-			{ name: '⏱️ 1M Timeout', value: '1M-T' },
-			{ name: '🚸 Warn', value: 'WARN' },
-		]
-	}, {
-		description: 'Reason for punishment',
-		name: 'reason', type: Option.String, required: true
-	}],
-	description: '⛔ Log the punishment of a member.',
-	execute: async interaction => {
-		const tag = interaction.data.options[0].value;
-		const punishment = interaction.data.options[1].value;
-		const reason = interaction.data.options[2].value;
-		const audit = (await Database.get('audit')) || [];
-		audit.push({ tag, punishment, reason });
-		await Database.set('audit', audit);
-		return card('Punish',
-			`:no_entry: Punished \`${tag}\` with \`${punishment}\` for:\n> ` +
-			reason + '\nhttps://ep.cristian-98.repl.co/audit/'
-		);
+		}\` total members.`, undefined, true);
 	}
 });
 
@@ -75,7 +37,7 @@ prefix({ // TODO: make it a command?
 			return info('Task Command', 'Type `' + Prefix + 'task <name>` to execute a task.');
 		for (const name of message.arguments) {
 			tasks[name].execute(message.bot);
-			return card('Task Command', `${tasks[name].emoji} Task \`${name}\` executed successfully.`);
+			return info('Task Command', `${tasks[name].emoji} Task \`${name}\` executed successfully.`);
 		}
 		return error('Task Command', `Task \`${message.arguments[0]}\` not found.`);
 	}

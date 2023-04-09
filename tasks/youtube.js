b8b6280e4f9e0d5336ebb3eda5e6c1b117c860d5
cristian-5 <cristianantonuccio@icloud.com>
2023-04-09 22:50:54 +0200

    emoji fixes

--- tasks/youtube.js
+++ tasks/youtube.js
@@ -1,22 +1,22 @@
 
 import {
 	getVideosAfterDate, composeURL, composeSHARE
-} from '../components/youtube.js';
-import { Zach, Time, Channels, Roles } from '../config.js';
-import { createTask, send, publish, text, card } from '../parser.js';
-import { Database } from '../database.js';
+} from "../components/youtube.js";
+import { Zach, Time, Channels, Roles } from "../config.js";
+import { createTask, send, publish, text, card } from "../parser.js";
+import { Database } from "../database.js";
 
-const YouTube = { color: 0xFF0000, emoji: '🔻' };
+const YouTube = { color: 0xFF0000, emoji: '❤️‍🔥' };
 
 createTask({
-	name: 'youtube', emoji: ':tv:', interval: Time.minutes(30),
-	description: 'Notifies members when a new YT video is out.',
+	name: "youtube", emoji: "📺", interval: Time.minutes(30),
+	description: "Notifies members when a new YT video is out.",
 	execute: async () => {
-		const date = await Database.get('youtube'); // get date of last video
+		const date = await Database.get("youtube"); // get date of last video
 		if (date == null) return;
 		const videos = await getVideosAfterDate(
-			Deno.env.get('YOUTUBE_KEY'),
-			Deno.env.get('YOUTUBE_CHANNEL'),
+			Deno.env.get("YOUTUBE_KEY"),
+			Deno.env.get("YOUTUBE_CHANNEL"),
 			date
 		);
 		if (videos == null) return;
@@ -24,19 +24,19 @@ createTask({
 			try {
 				const url = composeURL(video.id.videoId);
 				const m = await send(Channels.notifications, text(
-					`:heart_on_fire: Hey guys, check out <@${Zach}>'s new <@&${Roles.youtube}> video!\n${url}`
+					`❤️‍🔥 Hey guys, check out <@${Zach}>'s new <@&${Roles.youtube}> video!\n${url}`
 				));
 				publish(Channels.notifications, m.id);
-				Database.set('yt_video_link', composeSHARE(video.id.videoId));
-				Database.set('yt_video_title', video.snippet.title);
+				Database.set("yt_video_link", composeSHARE(video.id.videoId));
+				Database.set("yt_video_title", video.snippet.title);
 			} catch {
 				send(Channels.dev_chat, card(
-					'YouTube video detection task',
+					"YouTube video detection task",
 					`${YouTube.emoji} <@&${Roles.developer}>s, the yt api hates us!`,
 					YouTube.color
 				));
 			}
 		}
-		Database.set('youtube', (new Date()).toISOString());
+		Database.set("youtube", (new Date()).toISOString());
 	}
 });

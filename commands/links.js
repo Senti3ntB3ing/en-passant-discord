399780848d4249b44aea935961acf48d362d59a6
Cristian-A <cristianantonuccio@icloud.com>
2023-04-17 12:41:51 +0200

    ephemeral fix

--- commands/links.js
+++ commands/links.js
@@ -1,60 +1,60 @@
 
-import { Option, command, card } from '../parser.js';
+import { Option, command, card } from "../parser.js";
 
 const links = {
-	'Instagram': {
-		url: 'https://www.instagram.com/thechessnerd/',
-		color: 0xFFFFFF, emoji: '🌌',
-		description: 'Link to @thechessnerd Instagram page',
+	"Instagram": {
+		url: "https://www.instagram.com/thechessnerd/",
+		color: 0xFFFFFF, emoji: "🌌",
+		description: "Link to @thechessnerd Instagram page",
 	},
-	'Youtube': {
-		url: 'https://www.youtube.com/c/thechessnerd', 
-		color: 0xFF0000, emoji: '🔻',
-		description: 'Link to __thechessnerd__ Youtube channel',
+	"Youtube": {
+		url: "https://www.youtube.com/c/thechessnerd", 
+		color: 0xFF0000, emoji: "🔻",
+		description: "Link to __thechessnerd__ Youtube channel",
 	},
-	'Twitch': {
-		url: 'https://www.twitch.tv/thechessnerdlive/',
-		color: 0x9047FF, emoji: '💎',
-		description: 'Link to __thechessnerd__ Twitch Live',
+	"Twitch": {
+		url: "https://www.twitch.tv/thechessnerdlive/",
+		color: 0x9047FF, emoji: "💎",
+		description: "Link to __thechessnerd__ Twitch Live",
 	},
-	'tiktok': {
-		url: 'https://vm.tiktok.com/ZML7UdM8j/',
-		color: 0xFE2C57, emoji: '🎵',
-		description: 'Link to __thechessnerd__ tiktok',
+	"tiktok": {
+		url: "https://vm.tiktok.com/ZML7UdM8j/",
+		color: 0xFE2C57, emoji: "🎵",
+		description: "Link to __thechessnerd__ tiktok",
 	},
-	'Twitter': {
-		url: 'https://twitter.com/thechessnerd',
-		color: 0x1D9BF0, emoji: '🐦',
-		description: 'Link to @thechessnerd Twitter page',
+	"Twitter": {
+		url: "https://twitter.com/thechessnerd",
+		color: 0x1D9BF0, emoji: "🐦",
+		description: "Link to @thechessnerd Twitter page",
 	},
-	'Discord': {
-		url: 'https://discord.com/invite/DKHBFF22TJ/',
-		color: 0x5765F3, emoji: '👑',
-		description: 'Invite link to *thechessnerd* Discord',
+	"Discord": {
+		url: "https://discord.com/invite/DKHBFF22TJ/",
+		color: 0x5765F3, emoji: "👑",
+		description: "Invite link to *thechessnerd* Discord",
 	},
-	'Chess.com': {
-		url: 'https://www.chess.com/club/thechessnerd-exclusive-club',
-		color: 0x7FA650, emoji: '🧩',
-		description: 'Invite link to the **chess.com** club',
+	"Chess.com": {
+		url: "https://www.chess.com/club/thechessnerd-exclusive-club",
+		color: 0x7FA650, emoji: "🧩",
+		description: "Invite link to the **chess.com** club",
 	},
-	'reddit': {
-		url: 'https://www.reddit.com/r/thechessnerd/',
-		color: 0xFF4500, emoji: '🤖',
-		description: 'Link to __/r/thechessnerd__ reddit',
+	"reddit": {
+		url: "https://www.reddit.com/r/thechessnerd/",
+		color: 0xFF4500, emoji: "🤖",
+		description: "Link to __/r/thechessnerd__ reddit",
 	},
-	'Drop': {
-		url: 'https://thechessnerd.com/',
-		color: 0x37777F, emoji: '🛍',
-		description: 'Link to __thechessnerd co.__ merch drops',
+	"Drop": {
+		url: "https://thechessnerd.com/",
+		color: 0x37777F, emoji: "🛍",
+		description: "Link to __thechessnerd co.__ merch drops",
 	},
 };
 
 command({
-	name: 'links', emoji: ':link:',
-	description: '🔗 List of useful links.',
+	name: "links", emoji: ":link:",
+	description: "🔗 List of useful links.",
 	options: [{
-		description: 'Specific platform link to display',
-		name: 'platform', type: Option.String,
+		description: "Specific platform link to display",
+		name: "platform", type: Option.String,
 		required: false, choices: Object.keys(links).map(
 			name => ({ name: `${links[name].emoji} ${name}`, value: name })
 		),
@@ -66,12 +66,12 @@ command({
 			return card(
 				links[name].description,
 				`${links[name].emoji} ${links[name].url}`,
-				links[name].color
+				links[name].color, true // ephemeral
 			);
 		} else return {
 			embeds: [{
-				type: 'rich',
-				title: '@thechessnerd Community Links',
+				type: "rich",
+				title: "@thechessnerd Community Links",
 				color: 0xFFFFFF,
 				fields: Object.keys(links).map(name => ({
 					name: `${links[name].emoji} **${name}**:`,
@@ -86,21 +86,21 @@ command({
 });
 
 command({
-	name: 'schedule', emoji: ':calendar_spiral:', options: [],
-	description: '📅 Link to @thechessnerd Twitch schedule.',
+	name: "schedule", emoji: ":calendar_spiral:", options: [],
+	description: "📅 Link to @thechessnerd Twitch schedule.",
 	execute: () => card(
-		'Link to thechessnerd Twitch schedule',
-		':calendar_spiral: ' + links['Twitch'].url + 'schedule',
-		links['Twitch'].color
+		"Link to thechessnerd Twitch schedule",
+		":calendar_spiral: " + links["Twitch"].url + "schedule",
+		links["Twitch"].color, true
 	)
 });
 
 command({
-	name: 'invite', emoji: ':tickets:', options: [],
-	description: '🎫 Invite link to @thechessnerd Discord.',
+	name: "invite", emoji: ":tickets:", options: [],
+	description: "🎫 Invite link to @thechessnerd Discord.",
 	execute: () => card(
-		'Invite link to __thechessnerd__ Discord.',
-		':tickets: ' + links['Discord'].url,
-		links['Discord'].color
+		"Invite link to __thechessnerd__ Discord.",
+		":tickets: " + links["Discord"].url,
+		links["Discord"].color, true
 	)
 });
